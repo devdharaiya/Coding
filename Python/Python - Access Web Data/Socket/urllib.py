b64cf7d03bd01cc42a3d9a02c08c96b2405ecf74
Dev Dharaiya <64013154+devdharaiya@users.noreply.github.com>
2024-03-23 08:23:21 +0000

    Updates

--- Python/Python - Access Web Data/Socket/urllib.py
+++ Python/Python - Access Web Data/Socket/urllib.py
@@ -1,4 +1,5 @@
-import urllib.request
-fhandle = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-for line in fhandle:
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen("http://data.pr4e.org/intro-short.txt")
+for line in fhand:
     print(line.decode().strip())
