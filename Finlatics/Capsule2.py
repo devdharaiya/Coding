a704625f35f50ea87e65c98527e3a962083192c7
Dev Dharaiya <64013154+devdharaiya@users.noreply.github.com>
2024-06-24 15:43:41 +0000

    Finlatics

---
+++ Finlatics/Capsule2.py
@@ -0,0 +1,11 @@
+# Q1) Write a program that prompts the user to input a year, checks whether it's a leap year or not, and then prints the result.
+# while True:    
+#     raw = input("Enter the Year to check:")
+#     year = int(raw)
+#     if year % 4 == 0:
+#         print("Year:", year)
+#         print("Leap Year:", "Yes")
+#     else:
+#         print("Year:", year)
+#         print("Leap Year:", "No")
+
