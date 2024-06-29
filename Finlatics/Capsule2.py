6bda3b42eaeb89cdc8c0ee275e943a4e894f2177
Dev Dharaiya <64013154+devdharaiya@users.noreply.github.com>
2024-06-29 13:55:33 +0000

    new

--- Finlatics/Capsule2.py
+++ Finlatics/Capsule2.py
@@ -1,4 +1,4 @@
-# Q1) Write a program that prompts the user to input a year, checks whether it's a leap year or not, and then prints the result.
+## Q1) Write a program that prompts the user to input a year, checks whether it's a leap year or not, and then prints the result.
 # while True:    
 #     raw = input("Enter the Year to check:")
 #     year = int(raw)
@@ -9,3 +9,16 @@
 #         print("Year:", year)
 #         print("Leap Year:", "No")
 
+
+## Q2) Write a Python program that prompts the user to input a word. The program should then determine and output the count of vowels (a, e, i, o, u) in the provided word. Additionally, consider that the word can be in either uppercase or lowercase.
+# word = input("Please enter a word:")
+# check = word.lower()
+# count = 0
+# vowels = ['a','e','i','o','u']
+# for i in check:
+#     if i in vowels:
+#         count = count + 1
+# print("No. of vowels in given word:", count)
+
+
+## Q3) Write a Python program that allows the user to input a list of 6 names. After receiving the list, the program should print only the names that start with the letter 'a', regardless of whether the letter is uppercase or lowercase.
