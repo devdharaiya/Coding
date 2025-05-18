ac2e77587e15baead5b3d8261949f1f7c496b762
Dev Dharaiya <64013154+devdharaiya@users.noreply.github.com>
2025-05-18 17:11:06 +0530

    commit

--- Coursera/Python - Data Structures/6.Strings/strings.py
+++ Coursera/Python - Data Structures/6.Strings/strings.py
@@ -1,8 +1,11 @@
-fruit = "Banana"
-for sound in fruit:
-    print(sound)
+# fruit = "Banana"
+# for sound in fruit:
+#     print(sound)
 
 # -------------------------------------------------------------------------------------------------
 
-fruit = "banana"
-"bn" in fruit
+# fruit = "banana"
+# "bn" in fruit
+
+for i in range(1,11):
+    print(i)
--- Coursera/Python - Data Structures/8.Lists/Assignment8.4.py
+++ Coursera/Python - Data Structures/8.Lists/Assignment8.4.py
@@ -1,13 +1,17 @@
 # Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. When the program completes, sort and print the resulting words in python sort() order as shown in the desired output. You can download the sample data at http://www.py4e.com/code3/romeo.txt
 
-fopen = open("Python/Python - Data Structures/8.Lists/romeo.txt")
-fhandle = fopen.read()
-delim = fhandle.split()
-lime = list()
-for check in delim:
-    if check not in lime:
-        lime.append(check)
-    else:
-        continue
-lime.sort()
-print(lime)
+# fopen = open("devdharaiya/Coding/Coursera/Python - Data Structures/8.Lists/Assignment8.4.py")
+# fhandle = fopen.read()
+# delim = fhandle.split()
+# lime = list()
+# for check in delim:
+#     if check not in lime:
+#         lime.append(check)
+#     else:
+#         continue
+# lime.sort()
+# print(lime)
+
+
+
+
